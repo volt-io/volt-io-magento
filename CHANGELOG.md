4c070880d6a5c715202e845afa68ed0d15148c56
Krzysztof Graman <krzysztof@devmouse.pl>
2023-09-13 12:08:30 +0200

    VOLT-17 Update API structure

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,7 @@
 # What changed in Volt: Pay by Bank Module?
 
+## 1.1.0
+* Change API structure.
+
 ## 1.0.0
 * First release for Magento 2.3 and 2.4
--- Gateway/Request/CustomerDataBuilder.php
+++ Gateway/Request/CustomerDataBuilder.php
@@ -4,15 +4,16 @@ declare(strict_types=1);
 
 namespace Volt\Payment\Gateway\Request;
 
+use Magento\Payment\Gateway\Data\AddressAdapterInterface;
 use Magento\Payment\Gateway\Request\BuilderInterface;
 use Volt\Payment\Gateway\SubjectReader;
 
 class CustomerDataBuilder implements BuilderInterface
 {
     /**
-     * The payer’s details.
+     * Information about the shopper
      */
-    public const PAYER = 'payer';
+    public const SHOPPER = 'shopper';
 
     /**
      * This should uniquely identify a payer in your system,
@@ -28,10 +29,19 @@ class CustomerDataBuilder implements BuilderInterface
     public const EMAIL = 'email';
 
     /**
-     * The payer’s full name, up to 255 characters.
-     * Optional.
+     * Shopper's organisation name (required when firstName and lastName not provided).
      */
-    public const NAME = 'name';
+    public const ORGANISATION_NAME = 'organisationName';
+
+    /**
+     * Shopper's first name (required when lastName provided).
+     */
+    public const FIRST_NAME = 'firstName';
+
+    /**
+     * Shopper's last name (required when firstName provided).
+     */
+    public const LAST_NAME = 'lastName';
 
     /***
      * The payer’s IP address, in IPV4 format (xxx.xxx.xxx.xxx).
@@ -60,12 +70,27 @@ class CustomerDataBuilder implements BuilderInterface
         $customerId = $billingAddress->getCustomerId()
             ?? 'G' . $this->subjectReader->readOrderIncrementId($buildSubject);
 
-        return [
-            self::PAYER => [
-                self::REFERENCE => $customerId,
-                self::EMAIL => $billingAddress->getEmail(),
-                self::NAME => $billingAddress->getFirstname() . ' ' . $billingAddress->getLastname(),
-            ],
+        $shopper = [
+            self::REFERENCE => $customerId,
+            self::EMAIL => $billingAddress->getEmail(),
         ];
+
+        return [
+            self::SHOPPER => $this->injectCustomerName($shopper, $billingAddress),
+        ];
+    }
+
+    protected function injectCustomerName(array $data, AddressAdapterInterface $address): array
+    {
+        if (!empty($address->getCompany())) {
+            $data[self::ORGANISATION_NAME] = $address->getCompany();
+        }
+
+        if (!empty($address->getFirstname())) {
+            $data[self::FIRST_NAME] = $address->getFirstname();
+            $data[self::LAST_NAME] = $address->getLastname();
+        }
+
+        return $data;
     }
 }
